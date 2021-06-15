02d8581a7839d5bf47ada2a5153d74c866dc0845
LucioOSilva <lucioosilva00@gmail.com>
2021-06-15 19:21:59 -0300

    added req7

---
+++ challenges/desafio7.js
@@ -0,0 +1,32 @@
+// use('aggregations');
+db.movies.aggregate([
+  { $match: {
+    languages: { $in: ["English"] },
+  } },
+
+  { $unwind: "$cast" },
+
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  } },
+
+  { $project: {
+    _id: 1,
+    numeroFilmes: 1,
+    mediaIMDB: { $round: ["$mediaIMDB", 1] },
+  } },
+
+  { $sort: {
+    numeroFilmes: -1,
+    _id: -1,
+  } },
+
+  // query para contar se temos exatamente os 47055 documentos
+  // { $group: {
+  //   _id: null,
+  //   somaDocs: { $sum: 1 }
+  // } },
+
+]);
