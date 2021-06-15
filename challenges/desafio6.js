e05ee9ec385247b4d92e11a1d01d68b3ca4324a4
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-15 17:52:26 -0300

    desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,6 +1,16 @@
 db.movies.aggregate([
-  { $group: { _id: { $expr: { awards: { $regex: /Won/i } } } } },
+  { $match: { awards: { $regex: /.*Won.*Oscar/ } } },
+  { $group: { _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+  } },
   { $project: {
-    maior_rating: { $max: "$imdb.rating" } },
-  },
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] },
+  } },
 ]);
