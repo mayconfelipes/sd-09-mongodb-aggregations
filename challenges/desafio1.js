6419d2d75078a3a12421e7ac335b54c6ebdd490a
Enio Nicael <enionicael@gmail.com>
2021-06-15 18:32:54 -0300

    Solucao do desafio 1.

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,22 @@
-console.log("Hello MongoDB :)")
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        {
+          $nor: [
+            { genres: "Crime" },
+            { genres: "Horror" },
+          ],
+        },
+        {
+          $or: [
+            { rated: "PG" },
+            { rated: "G" },
+          ],
+        },
+        { languages: { $all: ["English", "Spanish"] } },
+      ],
+    },
+  },
+]);
