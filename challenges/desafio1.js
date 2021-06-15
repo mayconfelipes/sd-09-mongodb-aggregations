d1edc8611bf21655ce5d0a2a8486bfc4dc07c217
Lucas Silva <martssilvapro@gmail.com>
2021-06-15 17:25:37 -0300

    adiciona desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,11 @@
- 
+db.movies.aggregate(
+  [
+    { $match: {
+      "imdb.rating": { $eq: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+    },
+  ],
+);
