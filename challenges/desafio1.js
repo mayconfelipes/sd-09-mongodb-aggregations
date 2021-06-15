2b16000f1997895ed250740091d7ddab73c4e12b
Christopher <chris.tryber@gmail.com>
2021-06-15 16:17:38 -0300

    Requisito 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,8 @@
-// db.movies.aggregate([{ $match: {"imdb.rating": {$gte: 7}} }]) ;
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    $nor: [{ genres: "Crime" }, { genres: "Horror" }],
+    rated: { $in: ["PG", "G"] },
+    $and: [{ languages: "English" }, { languages: "Spanish" }],
+  } },
+]);
