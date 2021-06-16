40306dcade1bc1aeced55b8ae152ca1aa5b16d91
Luciano_Lodi <lrlodi@gmail.com>
2021-06-16 16:37:00 -0300

    Requisitos 7 corrigido e 10 completo

---
+++ challenges/desafio10.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $addFields:
+      {
+        trip_duration:
+          { $subtract: ["$stopTime", "$startTime"] },
+      },
+  },
+  {
+    $group:
+      {
+        _id: "$usertype",
+        avg_trip_time:
+          {
+            $avg: { $divide: ["$trip_duration", 3600000] },
+          },
+      },
+  },
+  { $project:
+    {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$avg_trip_time", 2] },
+    },
+  },
+]);
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -18,47 +18,47 @@
 //         "Kevin Spacey",
 //         "George Clooney",
 //       ]] },
-//       total_favs: { $size: "$num_favs" },
 //     },
 //   },
 //   { $sort: { num_favs: -1 } },
+//   { $skip: 24 }
 // ]);
 
-db.movies.aggregate([
-  {
-    $match:
-    {
-      countries: "USA",
-      "tomatoes.viewer.rating": { $gte: 3 },
-    },
-  },
-  {
-    $addFields:
-    {
-      num_favs:
-        {
-          $setIntersection:
-          [
-            "$cast",
-            [
-              "Sandra Bullock",
-              "Tom Hanks",
-              "Julia Roberts",
-              "Kevin Spacey",
-              "George Clooney",
-            ],
-          ],
-        },
-    },
-  },
-  { $sort: { total_favs: -1 } },
-  {
-    $project:
-    {
-      _id: 0,
-      title: 1,
-      total_favs: { $size: "$num_favs" },
-      num_favs: 1,
-    },
-  },
-]);
+// db.movies.aggregate([
+//   {
+//     $match:
+//     {
+//       countries: "USA",
+//       "tomatoes.viewer.rating": { $gte: 3 },
+//     },
+//   },
+//   {
+//     $addFields:
+//     {
+//       num_favs:
+//         {
+//           $setIntersection:
+//           [
+//             "$cast",
+//             [
+//               "Sandra Bullock",
+//               "Tom Hanks",
+//               "Julia Roberts",
+//               "Kevin Spacey",
+//               "George Clooney",
+//             ],
+//           ],
+//         },
+//     },
+//   },
+//   {
+//     $project:
+//     {
+//       _id: 0,
+//       title: 1,
+//       total_favs: { $size: "$num_favs" },
+//       num_favs: 1,
+//     },
+//   },
+//   { $sort: { total_favs: -1 } },
+// ]);
