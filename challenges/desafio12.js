89ec38645cb861b0d555334a47e506dc485ceab1
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-16 01:03:25 -0300

    desafio 11 e 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,14 +1,7 @@
-db.trip.aggregate([
-  {},
-]);
-
 db.trips.aggregate([
-  { $group: {
-    _id: {
-      startStationName: "$startStationName",
-      dayOfWeek: "$dayOfWeek" },
-    total: { $sum: 1 } },
-  },
-  // { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 } } },
-  // { $limit: 1 },
+  { $match: { $expr: { $eq: [{ $dayOfWeek: "$startTime" }, 5] } } },
+  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
+  { $sort: { total: -1 } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } },
+  { $limit: 1 },
 ]);
