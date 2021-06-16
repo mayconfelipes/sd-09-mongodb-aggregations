478aee6ae415f68c538d58ad2439583124517a1d
johnealves <johne_af@hotmail.com>
2021-06-16 17:54:56 -0300

    challenge 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -8,16 +8,16 @@ db.movies.aggregate([
       maior_rating: { $max: "$imdb.rating" },
       menor_rating: { $min: "$imdb.rating" },
       media_rating: { $avg: "$imdb.rating" },
-      desvio_padrao: { $stdDevSamp: "$imdb.rating"  },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
   {
     $project: {
       _id: 0,
-      "maior_rating" : "$maior_rating",
-      "menor_rating" : "$menor_rating",
-      "media_rating" : { $round: ["$media_rating", 1] },
-      "desvio_padrao" : { $round: ["$desvio_padrao", 1] },
+      maior_rating: "$maior_rating",
+      menor_rating: "$menor_rating",
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
 ]);
