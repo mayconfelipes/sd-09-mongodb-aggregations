69dd11414e07d4b8ecbb98f14fcd032510d171c9
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-06-15 23:54:58 -0300

    task 7 corrections

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,23 +1,23 @@
+use("aggregations");
 db.movies.aggregate([
-  { $match: { languages: "English" } },
+  {
+    $match: {
+      languages: "English",
+    },
+  },
   { $unwind: "$cast" },
-  { $group:
-    {
-      _id: "$cast",
-      numeroFilmes: { $sum: 1 },
-      mediaIMDB: { avg: "$imdb.rating" },
-    },
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
   },
-  {
-    $project: {
-      _id: 1,
-      numeroFilmes: 1,
-      mediaIMDB: {
-        $round: ["$mediaIMDB", 1],
-      },
-    },
   },
-  {
-    $sort: { numeroFilmes: -1, _id: -1 },
+  { $project: {
+    _id: 1,
+    numeroFilmes: 1,
+    mediaIMDB: {
+      $round: ["$mediaIMDB", 1] },
   },
+  },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
 ]);
