92c21123da59bc6781c673bd3e238bd674831689
Raquel Picanço <picanco.raquel@gmail.com>
2021-06-16 15:07:44 -0400

    Fix challenge 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -13,4 +13,15 @@ db.movies.aggregate([
       },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      title_split: 1,
+    },
+  },
+  {
+    $sort: {
+      title_split: 1,
+    },
+  },
 ]);
