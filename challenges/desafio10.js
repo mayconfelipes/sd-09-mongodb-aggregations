1afb17fa374769d7843202d5a7e7afecbd1e8c7c
Diogenes <diogenesfelipe.s@outlook.com>
2021-06-16 19:04:03 -0300

    feat/Desafio10

---
+++ challenges/desafio10.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      subtractTime: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      avarageMilliseconds: { $avg: "$subtractTime" }
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: [{ $divide: ["$avarageMilliseconds", 3600000]}, 2] },
+    },
+  }
+]);
