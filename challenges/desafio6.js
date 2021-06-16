6e65e1eb657e7e63718764dc5b1717bc7946144f
tiago <tiagopaz.dev@gmail.com>
2021-06-16 14:04:26 -0300

    req 6 and 7

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,25 @@
 db.movies.aggregate([
-  { $match: { awards: { $exists: true }, "imdb.rating": { $ne: "" } } },
-  { $project: { awards: 1, "imdb.rating": 1, teste: { $first: "$imdb.rating" } } },
-  { $sort: { "imdb.rating": -1 } },
+  {
+    $match: {
+      awards: { $regex: /Won\s\d+.Oscars?/ },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      mediaRating: { $avg: "$imdb.rating" },
+      desvioPadrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$mediaRating", 1] },
+      desvio_padrao: { $round: ["$desvioPadrao", 1] },
+    },
+  },
 ]);
