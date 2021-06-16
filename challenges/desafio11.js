0cc0c51a83953d2d961e0b40b3772bfaa28eb0d2
LucioOSilva <lucioosilva00@gmail.com>
2021-06-16 13:00:08 -0300

    added req11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,1 +1,17 @@
-// continuar aqui...
+// use('aggregations');
+db.trips.aggregate([
+  { $group: {
+    total: { $sum: 1 },
+    _id: { $dayOfWeek: "$startTime" },
+  } },
+
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: 1,
+  } },
+
+  { $sort: { total: -1 } },
+
+  { $limit: 1 },
+]);
