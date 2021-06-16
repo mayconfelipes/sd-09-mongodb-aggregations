aa5f27d3215d8b32c37b857178f8943a3c25c208
edison-ps <edison@osite.com.br>
2021-06-16 17:37:11 -0300

    Desafio 6 V1.00

---
+++ challenges/desafio6.js
@@ -0,0 +1,14 @@
+db.movies.aggregate([
+  { $match: {
+    $and: [{ awards: { $regex: /Won / } }, { awards: { $regex: /Oscar/ } }] } },
+  { $group: { _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" } } },
+  { $project: {
+    _id: false,
+    maior_rating: true,
+    menor_rating: true,
+    media_rating: { $round: ["$media_rating", 1] },
+    desvio_padrao: { $round: ["$desvio_padrao", 1] } } }]);
