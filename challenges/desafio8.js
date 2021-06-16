523da6df61b0540613aea9492943518a67acb044
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-16 13:44:47 -0300

    req 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,22 +1,19 @@
-db.air_routes.aggregate([
-  { $match: { airplane: { $in: ["747", "380"] } } },
-  { $lookup: {
-    from: "air_alliances",
-    localField: "airlines",
-    foreignField: "airline.name",
-    as: "alliances",
-  } },
-  { $sort: { alliances: -1 } },
-  { $limit: 1 },
-  // { $project: { _id: 0, alliances: 1 } },
-  // { $group: { _id: "$airlinesname", totalRotas: { $sum: 1 } } },
-]).pretty();
-
 db.air_alliances.aggregate([
   { $lookup: {
     from: "air_routes",
-    localField: "name",
-    foreignField: "airline.name",
+    let: { alliance_companies: "$airlines" },
+    pipeline: [{
+      $match: {
+        airplane: { $in: ["747", "380"] },
+        $expr: {
+          $in: ["$airline.name", "$$alliance_companies"],
+        },
+      },
+    }],
     as: "alliances",
   } },
-]).pretty();
+  // { $match: { "alliances.airplane": { $in: ["747", "380"] } } },
+  { $project: { _id: "$name", totalRotas: { $size: "$alliances" } } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
