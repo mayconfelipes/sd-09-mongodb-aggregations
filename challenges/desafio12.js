0d947d8fa955e644297cde807486fb7a46f8fdf7
johnealves <johne_af@hotmail.com>
2021-06-16 15:22:02 -0300

    fix challenge 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -18,4 +18,11 @@ db.trips.aggregate([
   },
   { $sort: { count: -1 } },
   { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id",
+      total: "$count",
+    },
+  },
 ]);
