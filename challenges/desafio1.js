b5fb4d95324951ce4ebf0469837e58f491fe395c
Hudson Carlos <hc88806@gmail.com>
2021-06-16 17:33:07 -0400

    challenge 1 done

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,4 +1,4 @@
 db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 },
-  $nor: [{genres: "Horror"}, {genres: "Crime"}],
+  $nor: [{ genres: "Horror" }, { genres: "Crime" }],
   $or: [{ rated: "G" }, { rated: "PG" }],
   languages: { $all: ["English", "Spanish"] } } }]);
