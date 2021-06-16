0fe03410b4ee78ec3e33b3adf94d0f81b728b371
Pablo Fernandes <pabloprates@gmail.com>
2021-06-16 12:30:11 -0300

    Challenge four

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+db.movies.aggregate(
+  [
+    {
+      $project: { title_split: { $split: ["$title", " "] }, _id: 0 },
+    },
+    {
+      $match:
+      {
+        title_split: { $size: 1 },
+      },
+    },
+  ],
+);
