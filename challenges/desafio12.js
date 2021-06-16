eb0a30c2575c7a5cf6d664cf7a53ab3c0880a0e9
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-06-16 12:22:05 -0300

    requirement 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,6 @@
 db.trips.aggregate([
-  { $group: { _id: { diaDaSemana: { $dayOfWeek: "$startTime" }, nomeDaEstacao: "$endStationName" }, total: { $sum: 1 } } },
-  { $match: { "_id.diaDaSemana": 5 } },
-  { $group: { _id: "$_id.nomeDaEstacao", total: { $sum: "$total" } } },
+  { $group: { _id: { diaDaSemana: { $dayOfWeek: "$startTime" }, nomeEstacao: "$startStationName" }, total: { $sum: 1 } } },
+  { $project: { _id: 0, nomeEstacao: "$_id.nomeEstacao", total: "$total" } },
   { $sort: { total: -1 } },
+  { $limit: 1 },
 ]);
