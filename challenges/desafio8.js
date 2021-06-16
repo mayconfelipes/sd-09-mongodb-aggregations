af11f46011f73af6e51f9a962ddcf10faf97510f
johnealves <johne_af@hotmail.com>
2021-06-16 20:12:06 -0300

    challenge 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,28 @@
+use("aggregations");
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: { airlineName: "$airlines" },
+      pipeline: [
+        {
+          $match: { 
+            airplane: { $in: ["747", "380"] },
+            $expr: { $eq: ["$airline.name", "$$airlineName"] }
+          }
+        }
+      ],
+      as: 'alience'
+    }
+  },
+  { $unwind: "$alience" },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 }
+    }
+  },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 }
+]);
