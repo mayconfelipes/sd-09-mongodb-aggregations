815e8eaf5a18bae86a2a69076ee1d85e06384058
tiago <tiagopaz.dev@gmail.com>
2021-06-16 15:48:19 -0300

    req 10

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -11,13 +11,13 @@ db.air_routes.aggregate([
     $match: { airplane: { $in: ["747", "380"] }, teste: { $gte: { size: 1 } } },
   },
   {
-    $group: { _id: "$teste.name", count: { $sum: 1 } },
+    $group: { _id: "$teste.name", totalRotas: { $sum: 1 } },
   },
   {
-    $project: { _id: 1, count: 1 },
+    $project: { _id: 1, totalRotas: 1 },
   },
   {
-    $sort: { count: -1 },
+    $sort: { totalRotas: -1 },
   },
   {
     $limit: 1,
