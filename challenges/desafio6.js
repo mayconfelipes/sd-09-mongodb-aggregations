272b48f21edcd0db17c4a05aab7db810d5c7ff4b
antonio arieiro <sfgfffs@gmail.com>
2021-06-17 16:23:36 -0300

    req 6 add project infos rating

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -13,4 +13,17 @@ db.movies.aggregate([
       desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: {
+        $round: ["$media_rating", 1],
+      },
+      desvio_padrao: {
+        $round: ["$desvio_padrao", 1],
+      },
+    },
+  },
 ]);
