bf530b40105d88c21f9b7f99597b513322a1ddbe
julianafatsil <julianafatsil@gmail.com>
2021-06-17 00:26:29 -0300

    Revision doublequote

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,10 +2,10 @@ db.movies.aggregate([
   {
     $match: {
       $and: [
-        { 'imdb.rating': { $gte: 7 } },
-        { genres: { $nin: ['Crime', 'Horror'] } },
-        { rated: { $in: ['PG', 'G'] } },
-        { $and: [{ languages: 'English' }, { languages: 'Spanish' }] },
+        { "imdb.rating": { $gte: 7 } },
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { $and: [{ languages: "English" }, { languages: "Spanish" }] },
       ],
     },
   },
