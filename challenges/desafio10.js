f1e260bd900b20e62ec4a104a94c79e51884e7df
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-06-16 21:06:52 -0300

    task 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,25 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      Media: {
+        $avg: {
+          $subtract: [
+            "$stopTime",
+            "$startTime",
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [{ $divide: ["$Media", 60 * 60 * 1000] }, 2],
+      },
+    },
+  },
+  { $sort: { duracaoMedia: 1 } },
+]);
