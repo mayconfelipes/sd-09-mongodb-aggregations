6f86afb1044277a40703971b716e4b88673a9cf4
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-06-16 22:15:42 -0300

    Challenge 6 code correction

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,14 +1,14 @@
 db.movies.aggregate([
   {
-    $match: { awards: { $regex: /ˆWon.*Oscar/i } },
+    $match: { awards: { $regex: /^Won.*Oscar/i } },
   },
   {
     $group: {
       _id: null,
-      maior_rating: { $max: "imdb.rating" },
-      menor_rating: { $min: "imdb.rating" },
-      media_rating: { $avg: "imdb.rating" },
-      desvio_padrao: { $stdDevSamp: "imdb.rating" },
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
   {
