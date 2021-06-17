d4370db2a1dae7b848ab49d7686ec01d46d14cb9
cleber-teixeira <cleberlt@gmail.com>
2021-06-17 04:34:54 -0300

    Desafio 11 - Refatoração. :white_check_mark:

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -3,10 +3,11 @@ db.trips.aggregate([
     _id: { $dayOfWeek: "$startTime" },
     total: { $sum: 1 },
   } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
   { $project: {
     _id: 0,
     diaDaSemana: "$_id",
-    total: { $max: "$total" },
+    total: "$total",
   } },
-  { $limit: 1 },
 ]);
