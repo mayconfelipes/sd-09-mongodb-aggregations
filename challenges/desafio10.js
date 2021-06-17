af441eb592118ec1ac2a446cd9e67ee544c940d1
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-17 19:40:25 -0300

    10?

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,22 +1,25 @@
 db.trips.aggregate([
   {
-    $project: {
-      _id: null,
-      tipo: "$usertype",
-      duracaoMedia: { $subtract: ["$stopTime", "$startTime"] },
-    },
-  },
-  {
     $group: {
-      _id: "$tipo",
-      duracaoMedia: { $avg: "$duracaoMedia" },
+      _id: "$usertype",
+      media: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            3600000,
+          ],
+        },
+      },
     },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] },
+      duracaoMedia: { $round: ["$media", 2] },
     },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
   },
 ]);
