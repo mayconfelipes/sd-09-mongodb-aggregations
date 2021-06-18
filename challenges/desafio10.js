3910f8fa25987a4a4792603c5996f4a0a9d11bd5
edison-ps <edison@osite.com.br>
2021-06-18 14:59:06 -0300

    Desafio 10 V1.02

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
 db.trips.aggregate([
   { $group: { _id: "$usertype",
     duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000] } } } },
-  { $project: { _id: false, tipo: "$_id", duracaoMedia: { $round: ["$duracaoMedia", 2] } } }]); 
+  { $project: { _id: false, tipo: "$_id", duracaoMedia: { $round: ["$duracaoMedia", 2] } } }]);
