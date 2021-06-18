36f973af6b8155639fbcd3f61fcce80bf76cfea7
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-06-17 21:44:03 -0300

    refatorando codigo

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,5 @@
 db.trips.aggregate([
-  { $match: { startTime: { $gte: ISODate("2016-03-10T00:00:00.000Z"), $lt: ISODate("2016-03-11T00:00:00.000Z") } } },
+  { $match: { startTime: { $gte: ISODate("2016-03-10"), $lt: ISODate("2016-03-11") } } },
   { $group: { _id: null, time: { $avg: { $subtract: ["$stopTime", "$startTime"] } } } },
   { $project: { duracaoMediaEmMinutos: { $ceil: { $divide: ["$time", 60000] } }, _id: 0 } },
 ]);
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,7 +1,7 @@
 const favs = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
 
 db.movies.aggregate([
-  { $match: { countries: "USA", "tomatoes.viewer.rating": { $gte: 3 }, cast: { $exists: true } } },
+  { $match: { countries: "USA", "tomatoes.viewer.rating": { $gte: 3 }, cast: { $in: favs } } },
   { $addFields: { num_favs: { $size: { $setIntersection: ["$cast", favs] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { title: 1, _id: 0 } },
