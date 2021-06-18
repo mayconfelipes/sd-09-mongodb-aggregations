74cdd330cb64cdf22f91a7c67695c36a86d3e29b
edison-ps <edison@osite.com.br>
2021-06-18 18:14:26 -0300

    Desafio 14 V1.00

---
+++ challenges/desafio14.js
@@ -0,0 +1,6 @@
+db.trips.aggregate([
+  { $group: { _id: "$bikeid",
+    duracaoMedia: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000] } } } },
+  { $sort: { duracaoMedia: -1 } },
+  { $limit: 5 },
+  { $project: { _id: false, bikeId: "$_id", duracaoMedia: { $ceil: "$duracaoMedia" } } }]);
