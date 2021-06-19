c3c696502f49768e4b9a2f21f400eec0fa41df71
Thiago Granville <thiagogranville@gmail.com>
2021-06-18 21:21:07 -0300

    [FIX][DESAFIO 10][Aplica sort para exibir em ordem crescente]

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -9,7 +9,12 @@ db.trips.aggregate([
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 1000 * 60 * 60] }, 2] },
+      duracaoMedia: {
+        $round: [{ $divide: ["$duracaoMedia", 1000 * 60 * 60] }, 2],
+      },
     },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
   },
 ]);
