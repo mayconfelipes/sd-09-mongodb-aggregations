60c4ddcd3cb2da8c7a0bc6754d91dbe7fcfc220a
Renato Feitosa <pythaqua@gmail.com>
2021-06-20 03:40:48 -0300

    Solve 5th challenge

---
+++ challenges/desafio5.js
@@ -0,0 +1,49 @@
+const favoriteStars = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": {
+        $gte: 3,
+      },
+      cast: {
+        $in: favoriteStars,
+      },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: ["$cast", favoriteStars],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+]);
