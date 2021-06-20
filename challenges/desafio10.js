e162c0ca2bf54942f83269e8520a489838126743
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-06-20 10:36:16 -0300

    :alien: Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: { $subtract: ["$stoptTime", "$startTime"] },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [{ $divide: ["$duracaoMedia", 60 * 60000] }, 2],
+      },
+    },
+  },
+  { $sort: { duracaoMedia: 1 } },
+]);
