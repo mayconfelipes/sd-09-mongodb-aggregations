1d1e9eda3c1c96b6fb48780c91c2215528c83ee0
lcgoandete <lcgoandete@hotmail.com>
2021-06-20 19:42:36 -0300

    desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,23 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [{
+          $divide: ["$duracaoMedia", 60 * 60 * 1000],
+        }, 2],
+      },
+    },
+  },
+]);
