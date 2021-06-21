7a5059f6e483861299292b330aa2e3b837bd7af7
edison-ps <edison@osite.com.br>
2021-06-21 16:37:17 -0300

    Desafio 8 V2.00

---
+++ challenges/desafio8.js
@@ -0,0 +1,16 @@
+db.air_routes.aggregate([
+  { $match: { airplane: { $in: ["380", "747"] } } },
+  { $group: { _id: "$airline.name", routes: { $sum: 1 } } },
+  { $lookup: {
+    from: "air_alliances",
+    let: { airline: "$_id" },
+    pipeline: [
+      { $unwind: "$airlines" },
+      { $match: { $expr: { $eq: ["$airlines", "$$airline"] } } },
+      { $project: { _id: false, name: true } }],
+    as: "airAliance" } },
+  { $unwind: "$airAliance" },
+  { $sort: { "airAliance.name": -1 } },
+  { $group: { _id: "$airAliance.name", totalRotas: { $sum: "$routes" } } },
+  { $sort: { routes: -1 } },
+  { $limit: 1 }]);
