2ab55ad6ebb194c3a2b6f5c377bc832b46f86dcb
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-06-21 11:40:38 -0300

    Challenge 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,2 +1,21 @@
-// { $dayOfWeek: <dateExpression> }
-// { "diaDaSemana" : <dia_da_semana>, "total" : <total_de_viagens> }
+db.trips.aggregate([
+  {
+    $group: {
+      _id: { $dayOfWeek: "$startTime" },
+      trips: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { trips: -1 },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$trips",
+    },
+  },
+]);
