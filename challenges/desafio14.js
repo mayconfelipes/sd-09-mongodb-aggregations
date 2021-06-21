969c158b95bd3029aa7e8ece6671cbad9001e240
lcgoandete <lcgoandete@hotmail.com>
2021-06-21 13:15:03 -0300

    desafio 14 fix projecao campo bikeId

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -12,7 +12,7 @@ db.trips.aggregate([
   {
     $project: {
       _id: false,
-      bikeid: "$_id",
+      bikeId: "$_id",
       duracaoMedia: {
         $ceil: [{
           $divide: ["$duracaoMedia", 60 * 1000],
