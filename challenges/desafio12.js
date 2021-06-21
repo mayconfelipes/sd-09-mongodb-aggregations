1db4a5ed2da2fa7388c1d19401b8ee7d430b168d
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-06-21 13:22:24 -0300

    attempt 6

---
+++ challenges/desafio12.js
@@ -0,0 +1,20 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        diaDeInicio: { $dayOfWeek: "$startTime" },
+        nomeEstac: "$startStationName"
+      },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nomeEstac",
+      total: "$total"
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
