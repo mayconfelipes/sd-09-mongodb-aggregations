4f731a3ca680860a109325ec2f7ca6c8ec5c70e0
Thiago Granville <thiagogranville@gmail.com>
2021-06-21 10:27:01 -0300

    [DESAFIO 13]

---
+++ challenges/desafio13.js
@@ -0,0 +1,25 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00.000Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
