5f1b2dd5abb99017893654868f8f7612cb7a7882
Felipe <felipesantiagolds@gmail.com>
2021-06-21 19:42:17 -0300

    Desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,8 +2,19 @@ db.trips.aggregate([
   {
     $group: {
       _id: "$usertype",
-      duracao: { $sum: 1 },
-      duracaoMedia: { $avg: "$duracao" },
+      duracao: {
+        $avg: { $subtract: ["$stopTime", "$startTime"] },
+      },
     },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: [{ $divide: ["$duracao", 3600000] }, 2] },
+    },
+  },
+  {
+    $sort: { duracaoMedia: 1 },
   },
 ]);
