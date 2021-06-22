7dd419c6fcf2b5585b51586753035476c1665f4d
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-06-22 19:55:45 -0300

    Task 01 - lint: Comma and semicolon

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -3,10 +3,10 @@ db.movies.aggregate([
     $match: {
       $and: [
         { "imdb.rating": { $gte: 7 } },
-        { "genres": { $nin: ["Crime", "Horror"] } },
-        { "rated": { $in: ["PG", "G"] } },
-        { "languages": { $in: ["English", "Spanish"] } },
-      ]
-    }
-  }
-])
+        { genres: { $nin: ["Crime", "Horror"] } },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $in: ["English", "Spanish"] } },
+      ],
+    },
+  },
+]);
