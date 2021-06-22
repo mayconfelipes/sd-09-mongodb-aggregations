2d549bec3e8ffef8db02bf3e3a6c9a4fe4a42234
marcusWittho <witthoeft.mcs@hotmail.com>
2021-06-22 10:24:53 -0300

    Requisito 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gte: ISODate("2016-03-10") },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      media: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: { $divide: ["$media", 60 * 1000] } },
+    },
+  },
+]);
