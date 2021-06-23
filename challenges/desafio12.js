d4e1e85ced33db6ad47960041150126fe983688e
Kevin Fraga <kevinciolac@gmail.com>
2021-06-23 03:31:26 -0300

    desafios :one::one: e :one::two: fix

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -4,5 +4,5 @@ db.trips.aggregate([
   { $group: { _id: "$startStationName", total: { $sum: 1 } } },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { $project: { _id: 0, nomeEstacao: "$_id", total: 1 } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$total" } },
 ]);
