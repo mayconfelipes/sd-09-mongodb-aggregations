bcc5e556170906bc36ae9eb4d5ef2bb0b242bb45
Enio Nicael <enionicael@gmail.com>
2021-06-23 11:00:41 -0300

    Solucao do desafio 13.

---
+++ challenges/desafio13.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00Z"),
+        $lte: ISODate("2016-03-11T23:59:59Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: {
+        $ceil: { $divide: ["$duracaoMediaEmMinutos", 60000] },
+      },
+    },
+  },
+]);
