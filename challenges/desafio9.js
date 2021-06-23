59fb0c23ee4e9515ec5f63e39e3f0fad05adcf9a
Kevin Fraga <kevinciolac@gmail.com>
2021-06-23 01:49:07 -0300

    desafio :nine:

---
+++ challenges/desafio9.js
@@ -0,0 +1,11 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: true, $ne: "" } } },
+  {
+    $group: {
+      _id: 1,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  { $project: { _id: 0 } },
+]);
