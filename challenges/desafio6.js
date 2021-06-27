3946cf6d600d7f0c12ad2535fcb88e5389001801
RodrigoNeoEx <draja.acc@gmail.com>
2021-06-27 12:24:49 -0300

    aggregate with match, group, math props and stdDevSamp

---
+++ challenges/desafio6.js
@@ -0,0 +1,17 @@
+db.movies.aggregate([
+  { $match: { awards: { $regex: /Won\s\d+.Oscars?/ } } },
+  { $group: {
+    _id: null,
+    maior_rating: { $max: "$imdb.rating" },
+    menor_rating: { $min: "$imdb.rating" },
+    mediaRating: { $avg: "$imdb.rating" },
+    desvioPadrao: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: ["$mediaRating", 1] },
+    desvio_padrao: { $round: ["$desvioPadrao", 1] },
+  } },
+]);
