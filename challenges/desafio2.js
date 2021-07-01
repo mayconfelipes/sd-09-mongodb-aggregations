3e23d6a15d748a3bac82c4a5ee21583a5e3bb4c9
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-07-01 09:16:13 -0300

    Challenge two, fix lint max line

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,5 +1,9 @@
 /*
-Utilizando o mesmo pipeline anterior, retorne apenas os campos title, rated, imdb.rating, imdb.votes e year, modificando seus nomes para titulo, avaliado, notaIMDB, votosIMDB e ano, respectivamente.
+Utilizando o mesmo pipeline anterior, retorne
+apenas os campos title, rated, imdb.rating, imdb.
+votes e year, modificando seus nomes para titulo,
+avaliado, notaIMDB, votosIMDB e ano,
+respectivamente.
 */
 db.movies.aggregate([
   {
---
+++ challenges/desafio3.js
@@ -0,0 +1,49 @@
+/*
+Agora que você tem os campos essenciais, aplique
+mais um estágio na pipeline do desafio anterior que
+atenda a seguinte demanda:
+
+Retorne esses filmes ordenados por ano e nota IMDB
+de forma decrescente e por ordem alfabética.
+O resultado da sua query deve ter o seguinte formato:
+*/
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": {
+        $gte: 7,
+      },
+      genres: {
+        $nin: [
+          "Crime",
+          "Horror",
+        ],
+      },
+      rated: {
+        $in: [
+          "PG",
+          "G",
+        ],
+      },
+      languages: {
+        $all: [
+          "English",
+          "Spanish",
+        ],
+      },
+    },
+  },
+  {
+    $limit: 41,
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rating",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+]);
