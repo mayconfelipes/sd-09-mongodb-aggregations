af626a5297ca08254a9683b2d8e964757c4e32b6
Bob <wend3llsouza@gmail.com>
2021-07-08 09:39:36 -0300

    :package: desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: { birthYear: { $ne: "", $exists: true } },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
