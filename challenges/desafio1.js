4b3ee6b8e84a15b6bc3a30412d8aba2620cc9f15
Micaela Alves <micaela.alves93@gmail.com>
2021-07-10 01:01:35 -0300

    Req 1 fix

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,10 +1,6 @@
 db.movies.aggregate([
-  { $match:
-    { $and: [
-      { "imdb.rating": { $gte: 7 } },
-      { genres: { $nin: ["Crime", "Horror"] } },
-      { rated: { $in: ["Pg", "G"] } },
-      { languages: { $in: ["English", "Spanish"] } },
-    ] },
-  },
+  { $match: { "imdb.rating": { $gte: 7 } } },
+  { $match: { genres: { $nin: ["Crime", "Horror"] } } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
+  { $match: { languages: { $all: ["English", "Spanish"] } } },
 ]);
