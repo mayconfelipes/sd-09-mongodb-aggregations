740f06ac1e8a09591d96f03315df5396e9fa9f91
Alstent <rafaelasltent@gmail.com>
2021-07-13 17:26:34 +0200

    resolvendo lint

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,9 +2,9 @@ const favActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey
 
 db.movies.aggregate([
   { $match: {
-      countries: "USA",
-      "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $in: favActors },
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+    cast: { $in: favActors },
   } },
   { $addFields: {
     num_favs: { $size: { $setIntersection: ["$cast", favActors] } },
@@ -14,7 +14,7 @@ db.movies.aggregate([
     "tomatoes.viewer.rating": -1,
     title: -1,
   } },
-  { $project: { _id: 0, title: 1} },
+  { $project: { _id: 0, title: 1 } },
   { $skip: 24 },
-  { $limit: 1},
+  { $limit: 1 },
 ]);
