e9a8fb8e00d2886a68e50cb6bdfbef403a842d3e
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-07-13 15:55:14 -0300

    Realizados desafios do 6 ao 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  { $match: { languages: "English" } },
+  { $unwind: "$cast" },
+  {
+    $group: {
+
+      _id: "$cast",
+      mediaIMDB: { $avg: "$imdb.rating" },
+      numeroFilmes: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { numeroFilmes: -1, _id: -1 },
+  },
+  {
+    $project: {
+      _id: 1,
+      cast: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+]);
