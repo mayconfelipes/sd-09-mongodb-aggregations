be00ee13299f4171dc8ac99dd0be8df6c1160c0a
Cainan <cainan6697@gmail.com>
2021-07-13 16:04:33 -0300

    Desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,20 @@
+db.movies.aggregate([
+  { $match: { languages: { $all: ["English"] } } },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      mediaIMDB: { $avg: "imdb.rating" },
+      numeroFilmes: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      cast: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+]);
