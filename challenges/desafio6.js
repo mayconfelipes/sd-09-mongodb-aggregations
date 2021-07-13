dd3b824f9e8ce797a9ce80ee47cc34451d641f8b
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-07-13 20:11:36 -0300

    Fix  desafio 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -14,7 +14,7 @@ db.movies.aggregate([
       _id: 0,
       maior_rating: 1,
       menor_rating: 1,
-      media_padrao: { $round: ["$media_rating", 1] },
+      media_rating: { $round: ["$media_rating", 1] },
       desvio_padrao: { $round: ["$desvio_padrao", 1] },
     },
   },
