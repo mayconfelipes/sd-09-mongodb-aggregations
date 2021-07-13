043177521cf89e9bcbb88742ab9353c3bedef262
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-07-13 18:38:55 -0300

    Fix desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -5,13 +5,13 @@ db.trips.aggregate([
       total: { $sum: 1 },
     },
   },
-  { $sort: { total: -1 } },
-  { $limit: 1 },
   {
     $project: {
       _id: 0,
       diaDaSemana: "$_id",
-      total: 1,
+      total: "$total",
     },
   },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
 ]);
