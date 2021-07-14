4b0a4dea710cf0e7f0c4ea9a1d97e47151cc33fb
victorvalim <victor.valim.c@gmail.com>
2021-07-14 11:59:41 -0300

    Ajuste?

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
   {
     $match: {
       countries: "USA",
-      "tomatoes.viewer.rating": { $gte: 3 },
+      "tomatoes.viewer.rating": { $gt: 2 },
       cast: { $exists: true },
     },
   }, {
