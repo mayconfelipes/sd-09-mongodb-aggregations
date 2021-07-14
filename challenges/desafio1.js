7e9bbd45a2ae5c27bdc54a445ca13754c59ce775
Víctor Cançado <victor.cancado@gmail.com>
2021-07-13 22:22:57 -0300

    Adiciona requisitos 1 e 2

---
+++ challenges/desafio1.js
@@ -0,0 +1,8 @@
+db.movies.aggregate([{
+  $match: { $and: [
+    { "imdb.rating": { $gte: 7 } },
+    { genres: { $nin: ["Crime", "Horror"] } },
+    { $or: [{ rated: "PG" }, { rated: "G" }] },
+    { $and: [{ languages: "English" }, { languages: "Spanish" }] },
+  ] },
+}]);
