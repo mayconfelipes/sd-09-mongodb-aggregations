eb660b7c6566df691621e74a657f92bfb6ef9142
Micaela Alves <micaela.alves93@gmail.com>
2021-07-13 21:09:03 -0300

    Requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $exists: true } } },
+  { $match: { birthYear: { $ne: "" } } },
+  { $group: {
+    _id: null,
+    maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+    menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+  } },
+  { $project: { _id: 0 } },
+]);
